7b52c7c2663ac1a6da9500522f21a514fb78e3b7
Radek <radekie@wp.pl>
2023-05-23 21:40:12 +0200

    api link update

--- client/src/api/axiosClient.ts
+++ client/src/api/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import queryString from 'query-string';
 
-const baseURL = 'http://localhost:5000/api/';
+const baseURL = 'http://funny-memories-pub-api.vercel.app/api/';
+// const baseURL = 'http://localhost:5000/api/';
 
 const axiosClient = axios.create({
   baseURL,
